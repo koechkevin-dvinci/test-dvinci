9ec559dbeac3dc497252ecefa0482323735332dd
Kevin Koech <koechkevin92@gmail.com>
2021-08-20 21:39:52 +0300

    COD-39 Add theme variables, Use theme addone to switch theme

--- src/lib/components/ManagementCard/Components.js
+++ src/lib/components/ManagementCard/Components.js
@@ -7,8 +7,7 @@ import {
   Divider,
   CardHeader,
   Box,
-  Typography,
-  IconButton,
+  Typography
 } from '@material-ui/core';
 import { MoreVert, Add as AddIcon } from '@material-ui/icons';
 
@@ -22,43 +21,50 @@ export const HeaderComponent = ({ headerText }) => {
   return (
     <Box display="flex" justifyContent="space-between" alignItems="center">
       <Typography>{headerText}</Typography>
-      <IconButton sizeSmall aria-label="more">
-        <MoreVert />
-      </IconButton>
+      <MoreVert />
     </Box>
   );
 };
 
+export const BodyComponent = ({header, description }) => {
+  return <Box>
+    <Typography variant="h6">{header}</Typography>
+    <Typography color="textSecondary" variant="body2">{description}</Typography>
+  </Box>
+}
+
 export const FooterComponent = (props) => {
   const { text, avatars = [] } = props;
   return (
-    <Box display="flex" justifyContent="space-between" alignItems="center">
+    <Box style={{ flex: 1}} display="flex" justifyContent="space-between" alignItems="center">
       <Box display="flex" alignItems="center" style={{ flex: 1 }}>
         {avatars.map((user, index) => {
-          return <Avatar style={{ marginRight: 8 }} key={index} alt={user.altIcon} src={user.imgSrc}>
-            {!user.imgSrc && user.altIcon}
-          </Avatar>;
+          return (
+            <Avatar style={{ marginRight: 8 }} key={index} alt={user.altIcon} src={user.imgSrc}>
+              {!user.imgSrc && user.altIcon}
+            </Avatar>
+          );
         })}
         <Typography>{text}</Typography>
       </Box>
-      <IconButton sizeSmall aria-label="Add">
-        <AddIcon />
-      </IconButton>
+      <AddIcon />
     </Box>
   );
 };
 const ManagementCard = (props) => {
-  const { makeStyle, headerText, footerText, avatars, ...restProps } = props;
+  const { makeStyle, headerText, footerText, avatars, bodyHeader, bodyDescription, ...restProps } = props;
   const useStyles = makeStyles({ ...styles });
   const classes = useStyles();
   return (
     <Card className={classes.root} {...restProps}>
       <CardHeader subheader={<HeaderComponent headerText={headerText} />} />
-      <CardContent></CardContent>
-      <Divider />
-      <CardContent>
-        <FooterComponent avatars={avatars} text={footerText} />
+      <CardContent style={{ minHeight: 86 }}>
+        <BodyComponent header={bodyHeader} description={bodyDescription} />
       </CardContent>
+      <Divider />
+      <Box display="flex" alignItems="center">
+      <CardHeader style={{ flex: 1, minHeight: 72}} subheader={<FooterComponent avatars={avatars} text={footerText} />} />
+      </Box>
     </Card>
   );
 };
--- src/stories/ManagementCard.stories.jsx
+++ src/stories/ManagementCard.stories.jsx
@@ -1,49 +1,92 @@
 import React from 'react';
 
 import { ManagementCard } from '../lib';
+import Grid from '@material-ui/core/Grid';
 
 export default {
   title: 'Management Card',
   component: ManagementCard,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: { },
 };
 
 const Template = (args) => <ManagementCard {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  headerText: 'ADMIN',
+export const WithAvatarsAndText = Template.bind({});
+WithAvatarsAndText.args = {
+  headerText: 'SALES',
+  footerText: '13 Members',
+  bodyHeader: 'Massachusetts',
+  bodyDescription: 'MA',
+  avatars: [
+    {
+      imgSrc: '',
+      name: 'Tim Sherwood',
+      altIcon: 'TS',
+    },
+    {
+      imgSrc: '',
+      name: 'Tim Sherwood',
+      altIcon: 'AB',
+    },
+    {
+      imgSrc: '',
+      name: 'Tim Sherwood',
+      altIcon: 'CD',
+    },
+  ],
+};
+
+export const Install = Template.bind({});
+Install.args = {
+  headerText: 'INSTALL',
   footerText: '22 Members',
-  avatars: [{
-    imgSrc: '',
-    name: 'Tim Sherwood',
-    altIcon: 'TS'
-  }, {
-    imgSrc: '',
-    name: 'Tim Sherwood',
-    altIcon: 'AB'
-  }, {
-    imgSrc: '',
-    name: 'Tim Sherwood',
-    altIcon: 'CD'
-  }]
+  bodyHeader: 'Flow Home Energy',
+  bodyDescription: 'AZ, SC, CA, TX',
+  avatars: [
+    {
+      imgSrc: '',
+      name: 'Tim Sherwood',
+      altIcon: 'TM',
+    },
+  ],
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-
+export const TextOnly = Template.bind({});
+TextOnly.args = {
+  headerText: 'ADMIN',
+  footerText: '65 Members',
+  bodyHeader: 'ECEC',
+  bodyDescription: 'ST, ST, ST, ST, ST, ST, ST, ST, ST, ST, ST, ST,ST, ST, ST, ST, ST, ST, ST, ST, ST, ST, ST, ST,',
+  avatars: [],
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
+export const CardsInPage = () => {
+  return (
+    <Grid container spacing={3}>
+      <Grid container item xs={12} spacing={3}>
+        <Grid item xs={12} sm={6} md={4}>
+          <TextOnly {...TextOnly.args} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <WithAvatarsAndText {...WithAvatarsAndText.args} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Install {...Install.args} />
+        </Grid>
+      </Grid>
+      <Grid container item xs={12} spacing={3}>
+        <Grid item xs={12} sm={6} md={4}>
+          <WithAvatarsAndText {...WithAvatarsAndText.args} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Install {...Install.args} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <TextOnly {...TextOnly.args} />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+CardsInPage.args = {}
