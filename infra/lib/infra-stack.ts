8ea5c5e54bdaaeba5dc1b67058699d40bff45278
kldeb <karam.debly@gmail.com>
2021-08-20 22:29:41 -0400

    add amplify infra

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from '@aws-cdk/core';
+import * as amplify from "@aws-cdk/aws-amplify";
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // create amplify app
+    const amplifyApp = new amplify.App(this, "storybook", {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: "Dvinci1",
+        repository: "storybook",
+        oauthToken: cdk.SecretValue.secretsManager("dev/dvinci1/github-token"),
+      }),
+      basicAuth: amplify.BasicAuth.fromCredentials("dvinci1", cdk.SecretValue.secretsManager("dev/storybook/basicAuth")),
+      autoBranchDeletion: true,
+    });
+
+    // set the main branch
+    const developBranch = amplifyApp.addBranch("develop");
+
+    // add the rewrite rules 
+    amplifyApp.addCustomRule(amplify.CustomRule.SINGLE_PAGE_APPLICATION_REDIRECT);
+
+    
+  }
+}
