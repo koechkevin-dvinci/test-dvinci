0c121b200d7a233f0a135533deb29c3088a5be5d
Kevin Koech <koechkevin92@gmail.com>
2021-08-25 21:03:40 +0300

    COD-39 Add Tab component clean up

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,5 +4,17 @@ import Search from './components/Search';
 import Select from './components/Select';
 import Modal from './components/Modal';
 import { Tabs, Tab, TabPanel } from './components/Tabs';
+import { AutoCompleteComponent } from './components/Select/SelectComponent';
 
-export { Button, ManagementCard, Search, Select, Modal, Tabs, Tab, TabPanel, PageHeader };
+export {
+  Button,
+  ManagementCard,
+  Search,
+  Select,
+  Modal,
+  Tabs,
+  Tab,
+  TabPanel,
+  PageHeader,
+  AutoCompleteComponent as AutoComplete,
+};
--- src/stories/Select.stories.jsx
+++ src/stories/Select.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Select } from '../lib';
+import { AutoComplete, Select } from '../lib';
 
 const Template = (args) => <Select {...args} />;
 
@@ -8,19 +8,50 @@ Default.args = {
   label: 'Business Categories',
   native: true,
   value: 10,
-  options: [{
-    label: 'One',
-    value: 1,
-  },{
-    label: 'Ten',
-    value: 10,
-  },{
-    label: 'Twenty',
-    value: 20,
-  },{
-    label: 'Thirty',
-    value: 30,
-  }]
+  options: [
+    {
+      label: 'One',
+      value: 1,
+    },
+    {
+      label: 'Ten',
+      value: 10,
+    },
+    {
+      label: 'Twenty',
+      value: 20,
+    },
+    {
+      label: 'Thirty',
+      value: 30,
+    },
+  ],
+};
+
+const AutoCompleteFunc = (args) => <AutoComplete {...args} />;
+export const MoreOptions = AutoCompleteFunc.bind({});
+MoreOptions.args = {
+  options: [
+    {
+      label: 'One',
+      value: 1,
+    },
+    {
+      label: 'Ten',
+      value: 10,
+    },
+    {
+      label: 'Twenty',
+      value: 20,
+    },
+    {
+      label: 'Thirty',
+      value: 30,
+    },
+  ],
+  id: '1',
+  label: 'Country',
+  size: 'small'
 };
 
 export default {
